7afd1c1824a04aaac5c83b4e29dea22e8360222b
Tejas Pawar <112255875+Tejas9523@users.noreply.github.com>
2025-04-13 18:21:49 +0530

    Add files via upload

---
+++ Ass8/HotelServiceInterface.java
@@ -0,0 +1,11 @@
+package practical_8_java_RMI;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.Map;
+
+public interface HotelServiceInterface extends Remote {
+    boolean bookRoom(String guestName, int roomNumber) throws RemoteException;
+    boolean cancelBooking(String guestName) throws RemoteException;
+    Map<Integer, String> showBookedRoomDetails() throws RemoteException;
+    
+}
